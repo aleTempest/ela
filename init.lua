25e8d55efb51632be585c74078e3c41a1dee1608
ale <ale.edgedancer@gmail.com>
2025-10-17 03:06:45 -0600

    basics

---
+++ init.lua
@@ -0,0 +1,65 @@
+-- VimPlug
+local Plug = vim.fn['plug#']
+
+vim.call('plug#begin')
+  Plug 'nvim-mini/mini.nvim'
+  Plug 'nvim-mini/mini.pairs'
+  Plug 'nvim-mini/mini.move'
+  Plug 'nvim-mini/mini.surround'
+  Plug 'nvim-mini/mini.statusline'
+vim.call('plug#end')
+
+require'mini.pairs'.setup()
+require'mini.move'.setup()
+require'mini.surround'.setup()
+require'mini.statusline'.setup()
+-- require'mini.'.setup()
+
+-- Sincronizar el clipboard de nvim con el del sistema
+vim.o.clipboard = "unnamedplus"
+
+-- Números absolutos y relativos
+vim.o.number = true
+vim.o.relativenumber = true
+
+-- Tabs and Indentation
+vim.o.tabstop = 2         
+vim.o.softtabstop = 2     
+vim.o.expandtab = true    
+vim.o.smartindent = true  
+vim.o.shiftwidth = 2      
+
+vim.o.breakindent = true 
+
+vim.o.hlsearch = true
+vim.o.incsearch = true
+
+vim.o.wrap = true
+
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+vim.o.mouse = ""
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- External grep program configuration
+vim.o.grepprg = "rg --vimgrep"
+vim.o.grepformat = "%f:%l:%c:%m"
+
+vim.o.updatetime = 50
+
+vim.o.swapfile = false
+vim.o.backup = false
+vim.o.undofile = true
+
+vim.o.termguicolors = true
+
+vim.o.cursorline = true
+
+vim.o.scrolloff = 8
+
+vim.o.colorcolumn = "80"
+
+vim.o.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
