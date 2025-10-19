c8c62c949c899a8a67cd6ea2e48a2ef308e17dc2
ale <ale.edgedancer@gmail.com>
2025-10-18 22:06:40 -0600

    lsp keybinds

--- init.lua
+++ init.lua
@@ -6,18 +6,14 @@ vim.call('plug#begin')
   Plug 'nvim-mini/mini.pairs'
   Plug 'nvim-mini/mini.move'
   Plug 'nvim-mini/mini.statusline'
-  
   Plug 'nvim-lua/plenary.nvim'
   Plug('nvim-telescope/telescope.nvim', { tag = '0.1.8' })
-  
   Plug'maxmx03/solarized.nvim'
-  
   Plug 'folke/which-key.nvim'
   Plug('nvim-treesitter/nvim-treesitter', { branch = 'master' })
   Plug 'neovim/nvim-lspconfig'
   Plug 'williamboman/mason.nvim'
   Plug 'williamboman/mason-lspconfig.nvim'
-  
   Plug 'hrsh7th/nvim-cmp'
   Plug 'hrsh7th/cmp-nvim-lsp'
   Plug 'hrsh7th/cmp-buffer'
@@ -27,21 +23,48 @@ vim.call('plug#begin')
 vim.call('plug#end')
 
 local hostname = vim.fn.hostname()
+local hostType = 0
 local winDesktopName = "DESKTOP-C3BF2T4"
 
 vim.cmd[[colorscheme solarized]]
 
 if hostname ~= winDesktopName then
   vim.o.background = "light"
+else
+  hostType = 1
 end
 
 vim.g.mapleader = " "
+
+function getPluginNames(hostType)
+  local files = ""
+  if hostType > 0 then
+    files = io.popen("dir /b .\\lua\\plugins"):read("*a")
+  else
+    files = io.popen("ls -a ./lua/plugins"):read("*a")
+  end
+  return files
+end
+
+local plugins = getPluginNames(hostType)
+print(plugins)
 
 require'plugins.telescope'.setup()
 require'plugins.mini'.setup()
 require'plugins.treesitter'.setup()
 require'plugins.lsp'.setup()
 
+-- Highlight al cursor cuando se copia algo
+vim.api.nvim_create_autocmd('TextYankPost', {
+  desc = 'Highlight when yanking (copying) text',
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+  callback = function()
+    vim.hl.on_yank()
+  end,
+})
+
+-- Yes
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Sincronizar el clipboard de nvim con el del sistema
 vim.o.clipboard = "unnamedplus"
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,108 +7,86 @@ local cmp = require('cmp')
 local luasnip = require('luasnip')
 
 M.setup = function()
-  mason.setup()
-  mason_lspconfig.setup({
-    ensure_installed = {
-      "lua_ls",
-      "pyright",
-    },
-    handlers = {
-      mason_lspconfig.default_setup, 
-      ['lua_ls'] = function()
-        lspconfig.lua_ls.setup {
-          settings = {
-            Lua = {
-              workspace = { checkThirdParty = false },
-              telemetry = { enable = false },
-            },
-          },
-        }
-      end,
-    }
+  vim.api.nvim_create_autocmd('LspAttach', {
+    group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+    callback = function(event)
+      local map = function(keys, func, desc, mode)
+        mode = mode or 'n'
+        vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+      end
+
+      map('grn', vim.lsp.buf.rename, '[R]e[n]ame')
+      map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
+      map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+      map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+      map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+      map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+      map('gO', require('telescope.builtin').lsp_document_symbols, 'Open Document Symbols')
+      map('gW', require('telescope.builtin').lsp_dynamic_workspace_symbols, 'Open Workspace Symbols')
+      map('grt', require('telescope.builtin').lsp_type_definitions, '[G]oto [T]ype Definition')
+    end
   })
 
-  local on_attach = function(client, bufnr)
-    -- Enable completion in Insert mode with <C-x><C-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    mason.setup()
+    mason_lspconfig.setup({
+      ensure_installed = {
+        "lua_ls",
+        "pyright",
+      },
+    })
 
-    local buf_set_keymap = vim.api.nvim_buf_set_keymap
-    local opts = { noremap=true, silent=true }
+    vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
-    -- Keymaps for LSP functions
-    buf_set_keymap(bufnr, 'n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-    buf_set_keymap(bufnr, 'n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-    buf_set_keymap(bufnr, 'n', '<space>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
+    cmp.setup({
+      snippet = {
+        expand = function(args)
+          luasnip.lsp_expand(args.body)
+        end,
+      },
+      window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+      },
+
+      -- Keybindings
+      mapping = cmp.mapping.preset.insert({
+        ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- Scroll backward
+        ['<C-f>'] = cmp.mapping.scroll_docs(4),  -- Scroll forward
+        ['<C-Space>'] = cmp.mapping.complete(),  -- Show completion menu
+        ['<C-e>'] = cmp.mapping.abort(),         -- Close completion menu
+        ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept selected item
+
+        -- Use Tab to cycle through suggestions (optional, but popular)
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { 'i', 's' }),
+      }),
+
+      -- Sources to use, in order of preference
+      sources = cmp.config.sources({
+        { name = 'nvim_lsp' },    -- LSP source (most intelligent)
+        { name = 'luasnip' },     -- Snippets
+        { name = 'buffer' },      -- Words in current buffer
+        { name = 'path' },        -- File paths
+      }),
+    })
 
   end
 
-  vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 
-  cmp.setup({
-    -- Setup for the snippet engine
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body)
-      end,
-    },
-
-    -- Appearance/UI settings (optional)
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-
-    -- Keybindings
-    mapping = cmp.mapping.preset.insert({
-      ['<C-b>'] = cmp.mapping.scroll_docs(-4), -- Scroll backward
-      ['<C-f>'] = cmp.mapping.scroll_docs(4),  -- Scroll forward
-      ['<C-Space>'] = cmp.mapping.complete(),  -- Show completion menu
-      ['<C-e>'] = cmp.mapping.abort(),         -- Close completion menu
-      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept selected item
-
-      -- Use Tab to cycle through suggestions (optional, but popular)
-      ['<Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_next_item()
-        elseif luasnip.expand_or_jumpable() then
-          luasnip.expand_or_jump()
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
-      ['<S-Tab>'] = cmp.mapping(function(fallback)
-        if cmp.visible() then
-          cmp.select_prev_item()
-        elseif luasnip.jumpable(-1) then
-          luasnip.jump(-1)
-        else
-          fallback()
-        end
-      end, { 'i', 's' }),
-    }),
-
-    -- Sources to use, in order of preference
-    sources = cmp.config.sources({
-      { name = 'nvim_lsp' },    -- LSP source (most intelligent)
-      { name = 'luasnip' },     -- Snippets
-      { name = 'buffer' },      -- Words in current buffer
-      { name = 'path' },        -- File paths
-    }),
-  })
-
-end
-
-
-
-
-return M
+  return M
